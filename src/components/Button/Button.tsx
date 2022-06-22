963b20f0f8da90bc0524ea75120715c06fcb4f5e
Adam Sisk <adamsisk91@gmail.com>
2022-06-22 14:14:12 -0500

    adds css support

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Button.css';
 
 export interface ButtonProps {
   label: string;
 }
 
 function Button(props: ButtonProps): React.ReactElement {
-  return <button>{props.label}</button>;
+  return <button className='button'>{props.label}</button>;
 }
 
 export default Button;
